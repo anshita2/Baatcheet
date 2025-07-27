98446684a19e1e36378115a7ebcdcdde27886e35
anshita2 <anshita316@gmail.com>
2025-07-27 13:12:21 +0530

    minor change

--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { BiSearchAlt2 } from "react-icons/bi";
-import OtherUsers from './OtherUsers';
+import OtherUsers from './otherusers';
 import axios from "axios";
 import toast from "react-hot-toast";
 import {useNavigate} from "react-router-dom";
